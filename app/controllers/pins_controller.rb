db5637d64dc2d5fd57e7ab3516724242597b866a
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-11 12:30:25 -0500

    Erase

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,6 @@
 class PinsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :basic_auth
+
 
   def index
     @pins = Pin.all.order('created_at DESC')
@@ -19,12 +19,7 @@ class PinsController < ApplicationController
       render :new
     end
   end
-  def basic_auth
-    authenticate_or_request_with_http_basic do |email, token|
-      user = current_user
-      user.api_token == token && user.email == email
-    end
-  end
+
   private
     def pin_params
       params.require(:pin).permit(:title, :image_url)
