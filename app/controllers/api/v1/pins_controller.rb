facdc9edc7c8836a15b1904a4cfb68f2be42d5cc
sbassmed <sebastiancamilomarin@gmail.com>
2019-02-11 12:30:48 -0500

    Correcting Method's

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PinsController < ApplicationController
 
-before_action :restrict_access
+  before_action :restrict_access
 
   def index
     render json: Pin.all.order('created_at DESC')
@@ -23,17 +23,17 @@ before_action :restrict_access
     email = request.headers['HTTP_X_USER_EMAIL']
     token = request.headers['HTTP_X_API_TOKEN']
     user = User.find_by_email(email)
-    if User.find_by_email(email)
-      unless userapi_token == token
-        render status: "HTTP 401"
+    if user
+      unless user.api_token == token
+        head 401
       end
     else
-      render status: "HTTP 401"
+      head 401
     end
   end
 
   def pin_params
     params.require(:pin).permit(:title, :image_url)
   end
-  
+
 end
